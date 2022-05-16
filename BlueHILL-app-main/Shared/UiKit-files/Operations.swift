4de593a65e3f7e81159cfcbeaab150180f3a13a7
BTosty <tharvijs63@gmail.com>
2022-05-17 02:41:52 +0300

    Working BG tasks
    Background tasks execute every 10 mins

--- BlueHILL-app-main/Shared/UiKit-files/Operations.swift
+++ BlueHILL-app-main/Shared/UiKit-files/Operations.swift
@@ -12,8 +12,7 @@ import UserNotifications
 struct Operations{
     static func update() -> [Operation]{
         let test = test()
-        let test2 = test2()
-        return [test2, test]
+        return [test]
     }
 }
 
@@ -27,41 +26,57 @@ class test: Operation{
             let blue = BLEManager.shared
             if blue.isSwitchedOn{
                 blue.startScanning()
+                
                 print(">>>>> BLUETOOTH IS WORKING YAY")
+                
 
+                if blue.peripherals.count < 1{
+                    let content = UNMutableNotificationContent()
+                    content.title = "BChip finder"
+                    content.subtitle = "A BChip has been forgoten"
+                    content.sound = UNNotificationSound.default
+                    //le trigger
+                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                    //le id
+                    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                    //send it
+                    UNUserNotificationCenter.current().add(request)
+                }
+                
                 for device in blue.peripherals{
                     print(">>>>> \(device.name)")
                     if device.distance == "0-5"{
                         NSLog(device.name)
                         NSLog(">>>>>>>> TOO FAR")
-                        
+                        let content = UNMutableNotificationContent()
+                        content.title = "BChip finder"
+                        content.subtitle = "\(device.name) has been forgoten"
+                        content.sound = UNNotificationSound.default
+                        //le trigger
+                        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                        //le id
+                        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                        //send it
+                        UNUserNotificationCenter.current().add(request)
                     }
                 }
                 blue.stopScanning()
             }else{
                 NSLog(">>>>>>>> Bluetooth is off")
                 blue.stopScanning()
+                let content = UNMutableNotificationContent()
+                content.title = "BChip finder"
+                content.subtitle = "Turn on bluetooth!"
+                content.sound = UNNotificationSound.default
+                //le trigger
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                //le id
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                //send it
+                UNUserNotificationCenter.current().add(request)
             }
             
         }
     }
 }
 
-class test2: Operation{
-
-    override func main() {
-        do{
-            NSLog(">>>>>>>> ayuy")
-            let content = UNMutableNotificationContent()
-            content.title = "Warning"
-            content.subtitle = "x has been forgoten"
-            content.sound = UNNotificationSound.default
-            //le trigger
-            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-            //le id
-            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-            //send it
-            UNUserNotificationCenter.current().add(request)
-        }
-    }
-}

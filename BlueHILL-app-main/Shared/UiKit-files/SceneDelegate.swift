613d551b48e5f0a88b16cec5a83bc9fffcda58d8
BTosty <tharvijs63@gmail.com>
2022-05-17 11:41:50 +0300

    Notification change for BG
    Improved bg notif logic so that it replies to events better. After testing was succesful.

--- BlueHILL-app-main/Shared/UiKit-files/SceneDelegate.swift
+++ BlueHILL-app-main/Shared/UiKit-files/SceneDelegate.swift
@@ -20,7 +20,59 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
     func sceneDidEnterBackground(_ scene: UIScene) {
         NSLog(">>>>>>>> sceneDidEnterBackground")
+        NSLog(">>>>>>>> work tim")
+        let blue = BLEManager.shared
+        if blue.isSwitchedOn{
+            blue.startScanning()
+            
+            print(">>>>> BLUETOOTH IS WORKING YAY")
+            
 
+            if blue.peripherals.count < 1{
+                let content = UNMutableNotificationContent()
+                content.title = "BChip finder"
+                content.subtitle = "A BChip has been forgoten"
+                content.sound = UNNotificationSound.default
+                //le trigger
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                //le id
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                //send it
+                UNUserNotificationCenter.current().add(request)
+            }
+            
+            for device in blue.peripherals{
+                print(">>>>> \(device.name)")
+                if device.distance == "0-5"{
+                    NSLog(device.name)
+                    NSLog(">>>>>>>> TOO FAR")
+                    let content = UNMutableNotificationContent()
+                    content.title = "BChip finder"
+                    content.subtitle = "\(device.name) has been forgoten"
+                    content.sound = UNNotificationSound.default
+                    //le trigger
+                    let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                    //le id
+                    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                    //send it
+                    UNUserNotificationCenter.current().add(request)
+                }
+            }
+            blue.stopScanning()
+        }else{
+            NSLog(">>>>>>>> Bluetooth is off")
+            blue.stopScanning()
+            let content = UNMutableNotificationContent()
+            content.title = "BChip finder"
+            content.subtitle = "Turn on bluetooth!"
+            content.sound = UNNotificationSound.default
+            //le trigger
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+            //le id
+            let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+            //send it
+            UNUserNotificationCenter.current().add(request)
+        }
         sqeduleRef()
     }
     
